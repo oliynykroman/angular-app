e3b5d08df6778e4512accf8be0fda37f99a0e0e7
Roman oliynyk <oliynykroma@live.com>
2018-03-19 23:08:20 +0200

    install webpack

---
+++ module-greetings.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let hello = require('./hello');
+
+hello ('firstrun');
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+    entry: './hello',
+    output: {
+        filename: 'build.js'
+    }
+}
